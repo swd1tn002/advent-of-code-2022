97369f4d22d981658b2b0cf65535940cade8138a
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-12-01 18:37:21 +0000

    Renaming and refactoring

--- src/01_calories/elfCalories.ts
+++ src/01_calories/elfCalories.ts
@@ -12,21 +12,25 @@ import { splitLines } from '../utils/strings';
 let puzzleInput = readFileSync(__dirname + '/input.txt', 'utf-8');
 let elves = splitLines(puzzleInput, '\n\n');
 
-
-function countCalories(elve: string): number {
-    /* The Elves take turns writing down the number of Calories contained
-     * by the various meals, snacks, rations, etc. that they've brought with
-     * them, one item per line. */
-    let linesAsNumbers: number[] = splitLines(elve).map(Number);
+/**
+ * The Elves take turns writing down the number of Calories contained
+ * by the various meals, snacks, rations, etc. that they've brought with
+ * them, one item per line.
+ *
+ * @param elf multiline string containing the meals
+ * @returns number of calories
+ */
+function countCalories(elf: string): number {
+    let linesAsNumbers: number[] = splitLines(elf).map(Number);
     return sum(linesAsNumbers);
 }
 
 /* Find the Elf carrying the most Calories. How many
  * total Calories is that Elf carrying? */
-let elveCalories: number[] = elves.map(countCalories);
-console.log('Max calories', max(elveCalories));
+let elfCalories: number[] = elves.map(countCalories);
+console.log('Max calories:', max(elfCalories));
 
 /* Find the top three Elves carrying the most Calories.
  * How many Calories are those Elves carrying in total? */
-let sorted = sortNumbers(elveCalories);
-console.log('Top three calories', sum(sorted.slice(-3)));
+let topThree = sortNumbers(elfCalories).slice(-3);
+console.log('Top three calories total:', sum(topThree));
