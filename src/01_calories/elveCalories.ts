84522fc97c8c2f137ce3aa1ff004ef1d40fdb2d7
Teemu Havulinna <havulinna@users.noreply.github.com>
2022-12-01 16:19:55 +0000

    Better formatting

--- src/01_calories/elveCalories.ts
+++ src/01_calories/elveCalories.ts
@@ -1,12 +1,21 @@
 import { readFileSync } from 'fs';
 import { sum, max, sortNumbers } from '../utils/arrays';
 
+
+/* The Elves take turns writing down the number of Calories contained by the
+ * various meals, snacks, rations, etc. that they've brought with them, one
+ * item per line. Each Elf separates their own inventory from the previous Elf's
+ * inventory (if any) by a blank line. 
+ * 
+ * https://adventofcode.com/2022/day/1
+ */
 let puzzleInput = readFileSync(__dirname + '/input.txt', 'utf-8');
 let elves = puzzleInput.split('\n\n');
 
 
 function countCalories(elve: string): number {
-    return sum(elve.split('\n').map(Number));
+    let linesAsNumbers: number[] = elve.split('\n').map(Number);
+    return sum(linesAsNumbers);
 }
 
 /* Find the Elf carrying the most Calories. How many
