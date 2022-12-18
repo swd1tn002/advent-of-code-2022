142843c899d4db23b641e0738a7f154981823e7d
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-12-18 10:28:22 +0000

    Day 18 cleaned up

--- src/18_boiling_boulders/index.ts
+++ src/18_boiling_boulders/index.ts
@@ -1,98 +1,136 @@
 import path from 'path';
 import { readFileSync } from 'fs';
-import { splitLines, splitNumberMatrix } from '../utils/strings';
-import { max } from '../utils/arrays';
+import { splitLines } from '../utils/strings';
+import { max, min } from '../utils/arrays';
+import { Range } from '../utils/Range';
 
+class Point3D {
+    constructor(readonly x: number, readonly y: number, readonly z: number) { }
 
-const puzzleInput = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
-let boulderSet = new Set(splitLines(puzzleInput));
-let boulders = splitNumberMatrix(puzzleInput, '\n', ',');
-
-function countExteriorArea(boulders: number[][]) {
-    let count = 0;
-    for (let [x, y, z] of boulders) {
-        //console.log({ x, y, z });
-
-        let p1 = [x + 1, y, z].join(',')
-        let p2 = [x - 1, y, z].join(',')
-        let p3 = [x, y + 1, z].join(',')
-        let p4 = [x, y - 1, z].join(',')
-        let p5 = [x, y, z + 1].join(',')
-        let p6 = [x, y, z - 1].join(',')
-
-        Array.from([p1, p2, p3, p4, p5, p6]).forEach(element => {
-            if (!boulderSet.has(element)) {
-                count++;
-            }
-        });
+    neighbors(): Point3D[] {
+        let { x, y, z } = this;
+        return [
+            new Point3D(x + 1, y, z), new Point3D(x - 1, y, z),
+            new Point3D(x, y + 1, z), new Point3D(x, y - 1, z),
+            new Point3D(x, y, z + 1), new Point3D(x, y, z - 1)
+        ];
     }
-    return count;
+
+    static parse(xyz: string): Point3D {
+        let [x, y, z] = xyz.split(',').map(n => Number(n));
+        return new Point3D(x, y, z);
+    }
+
+    toString(): string {
+        return `${this.x},${this.y},${this.z}`;
+    }
 }
 
-function main() {
-    console.log('Part 1:', countExteriorArea(boulders));
-
-
-    let maxX = max(boulders.map(b => b[0]));
-    let maxY = max(boulders.map(b => b[1]));
-    let maxZ = max(boulders.map(b => b[2]));
-
-    let airSet = new Set<string>();
-    let outFacing = new Set<string>();
-
-    let unvisited = ['0,0,0'];
-
-    while (unvisited.length > 0) {
-        let xyz = unvisited.pop() as string;
-        airSet.add(xyz);
-
-        let [x, y, z] = xyz.split(',').map(i => Number(i));
-
-        let p1 = [x + 1, y, z].join(',');
-        let p2 = [x - 1, y, z].join(',');
-        let p3 = [x, y + 1, z].join(',');
-        let p4 = [x, y - 1, z].join(',');
-        let p5 = [x, y, z + 1].join(',');
-        let p6 = [x, y, z - 1].join(',');
-
-        Array.from([p1, p2, p3, p4, p5, p6]).forEach(element => {
-            if (x > maxX + 1 || y > maxY + 1 || z > maxZ + 1 || x < -1 || y < -1 || z < -1) {
-                return;
-            }
-            if (airSet.has(element)) {
-                // already visited
-                return;
-            }
-            if (boulderSet.has(element)) {
-                outFacing.add(element);
-                return;
-            }
-            // continue from that element later
-            unvisited.push(element);
-        });
+class Box3D {
+    constructor(readonly x: Range, readonly y: Range, readonly z: Range) {
     }
 
-    // fill air pockets
-    for (let x = 0; x <= maxX; x++) {
-        for (let y = 0; y <= maxY; y++) {
-            for (let z = 0; z <= maxZ; z++) {
-                let xyz = [x, y, z].join(',');
+    contains(point: Point3D): boolean {
+        return this.x.contains(point.x) && this.y.contains(point.y) && this.z.contains(point.z);
+    }
 
-                if (!boulderSet.has(xyz) && !airSet.has(xyz)) {
-                    // Air pocket!
-                    console.log('air pocket at', xyz);
-
-                    // fill it:
-                    boulderSet.add(xyz);
-                    boulders.push([x, y, z]);
+    *[Symbol.iterator]() {
+        for (let x = this.x.start; x <= this.x.end; x++) {
+            for (let y = this.y.start; y <= this.y.end; y++) {
+                for (let z = this.z.start; z <= this.z.end; z++) {
+                    yield new Point3D(x, y, z);
                 }
             }
         }
     }
+}
 
 
-    let part2 = countExteriorArea(boulders);
-    console.log({ count2: part2 }); // 2520
+function countExteriorArea(boulderSet: Set<string>) {
+    let points = Array.from(boulderSet).map(Point3D.parse);
+
+    // get all sides for all points, and count the ones that do not have a boulder as a neighbor
+    return points.flatMap(p => p.neighbors()).filter(side => {
+        let sideStr = `${side.x},${side.y},${side.z}`;
+        return !boulderSet.has(sideStr)
+    }).length;
+}
+
+/**  Returns a box that surrounds all given points. */
+function getOuterBounds(boulders: Set<string>): Box3D {
+    let coords = Array.from(boulders).map(xyz => xyz.split(',').map(n => Number(n)));
+    let x = coords.map(c => c[0]);
+    let y = coords.map(c => c[1]);
+    let z = coords.map(c => c[2]);
+
+    return new Box3D(
+        new Range(min(x) - 1, max(x) + 1),
+        new Range(min(y) - 1, max(y) + 1),
+        new Range(min(z) - 1, max(z) + 1)
+    );
+}
+
+function main() {
+    /* "Because of how quickly the lava is moving, the scan isn't very good; its resolution is quite low and, as a result,
+     * it approximates the shape of the lava droplet with 1x1x1 cubes on a 3D grid, each given as its x,y,z position. */
+    const puzzleInput = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
+    let boulderSet = new Set(splitLines(puzzleInput));
+
+    /* "To approximate the surface area, count the number of sides of each cube that are not immediately connected to another
+     * cube. So, if your scan were only two adjacent cubes like 1,1,1 and 2,1,1, each cube would have a single side covered
+     * and five sides exposed, a total surface area of 10 sides."
+     */
+    console.log('Part 1:', countExteriorArea(boulderSet));
+
+
+    /* "Something seems off about your calculation. The cooling rate depends on exterior surface area, but your calculation
+     * also included the surface area of air pockets trapped in the lava droplet."
+     *
+     * "Instead, consider only cube sides that could be reached by the water and steam as the lava droplet tumbles into the
+     * pond. The steam will expand to reach as much as possible, completely displacing any air on the outside of the lava
+     * droplet but never expanding diagonally."
+     */
+
+    let bounds = getOuterBounds(boulderSet);
+    let surroundingAir = new Set<string>();
+
+    /* Visit all air positions that can be accessed from the starting point outside the shape.
+     * All air positions other than these are air pockets inside the given shape. */
+    let unvisited = [new Point3D(bounds.x.start, bounds.y.start, bounds.z.start).toString()];
+
+    while (unvisited.length > 0) {
+        let next = unvisited.pop() as string;
+        surroundingAir.add(next);
+
+        Point3D.parse(next).neighbors().forEach(neighbor => {
+            if (!bounds.contains(neighbor)) {
+                return; // no air pockets can exist outside the bounds of the shape
+            }
+
+            let xyz = neighbor.toString();
+            if (surroundingAir.has(xyz) || boulderSet.has(xyz)) {
+                return; // the neighbor is already known, so no need to visit it
+            }
+
+            // add to queue to expand to later
+            unvisited.push(neighbor.toString());
+        });
+    }
+
+    // Iterate through all points in the surrounding box to find all points that are
+    // not found in the air around the boulder nor the boulder itself. Then fill those points.
+    let solidBoulder = new Set(boulderSet);
+
+    for (let point of bounds) {
+        let xyz = point.toString();
+
+        if (!solidBoulder.has(xyz) && !surroundingAir.has(xyz)) {
+            solidBoulder.add(xyz); // when an air pocket is found, fill it to make the shape solid!
+        }
+    }
+
+    let exteriorArea = countExteriorArea(solidBoulder);
+    console.log('Part 2:', exteriorArea); // 2520
 }
 
 main();
