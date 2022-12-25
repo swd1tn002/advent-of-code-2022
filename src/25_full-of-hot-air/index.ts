587a51043424455f0613b9e16dc6751d9d7e8c78
Teemu H <44917122+swd1tn002@users.noreply.github.com>
2022-12-25 18:34:29 +0000

    Cleaned up day 25

--- src/25_full-of-hot-air/index.ts
+++ src/25_full-of-hot-air/index.ts
@@ -5,34 +5,26 @@ import { sum } from '../utils/arrays';
 
 const puzzleInput = readFileSync(path.join(__dirname, 'input.txt'), 'utf-8');
 
-/**
- * "You know, I never did ask the engineers why they did that. Instead of using digits four through
- * zero, the digits are 2, 1, 0, minus (written -), and double-minus (written =).
- * Minus is worth -1, and double-minus is worth -2."
- */
-let digits: { [code: string]: number } = {
-    '2': 2,
-    '1': 1,
-    '0': 0,
-    '-': -1,
-    '=': -2
-};
 
 /**
  * "Okay, our Special Numeral-Analogue Fuel Units - SNAFU for short - are sort of like normal numbers.
  * You know how starting on the right, normal numbers have a ones place, a tens place, a hundreds place,
  * and so on, where the digit in each place tells you how many of that value you have?"
- *
- * "SNAFU works the same way, except it uses powers of five instead of ten. Starting from the right,
- * you have a ones place, a fives place, a twenty-fives place, a one-hundred-and-twenty-fives place,
- * and so on. It's that easy!"
- */
+*
+* "SNAFU works the same way, except it uses powers of five instead of ten. Starting from the right,
+* you have a ones place, a fives place, a twenty-fives place, a one-hundred-and-twenty-fives place,
+* and so on. It's that easy!"
+*/
 function parseSnafu(num: string): number {
-    // as numbers in reverse order
-    let digitsReversed = num.split('').reverse().map(x => digits[x]);
+    /**
+     * "You know, I never did ask the engineers why they did that. Instead of using digits four through
+     * zero, the digits are 2, 1, 0, minus (written -), and double-minus (written =).
+     * Minus is worth -1, and double-minus is worth -2."
+     */
+    let digits = num.split('').map(x => '=-012'.indexOf(x) - 2);
 
     // multiply each digit with the corresponding power of 5 and sum them together
-    return sum(digitsReversed.map((val, i) => val * Math.pow(5, i)));
+    return sum(digits.reverse().map((val, i) => val * Math.pow(5, i)));
 }
 
 function toSnafu(num: number): string {
